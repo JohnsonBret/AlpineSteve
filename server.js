8bb852a80ac9b53481fdf058771e921eea24effb
Bret Main <bretaudio@gmail.com>
2019-01-15 10:16:02 -0800

    removed commented code from server.js

--- server.js
+++ server.js
@@ -11,24 +11,6 @@ hbs.registerPartials(__dirname + "/views/partials");
 app.set('view engine', 'hbs');
 
 
-// app.use((req, res, next)=>{
-//     var now = new Date().toString();
-//     var log = `Method: ${req.method} Request url: ${req.url} at time: ${now}`;
-//     fs.appendFile('server.log', log + '\n', (err) =>{
-//         if(err){
-//             console.log("Unable to append server log");
-//         }
-//     });
-//     next();
-// });
-
-// app.use((req, res, next)=>{
-//     res.render('maintainence.hbs', {
-//         pageTitle: "We'll be Back!",
-//     });
-//     // next();
-// });
-
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
